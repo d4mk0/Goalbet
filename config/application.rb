2332deb5c34de51942cf4947e8b0258ebc4aa9e3
Damir Konkov <d4mk0f@gmail.com>
2014-07-12 22:52:20 +0400

    default time zone was changed to Moscow(+0400) and landing.css was added to precompilation assets

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module Goalbet
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ru
+    config.time_zone = 'Moscow'
+
+    config.assets.precompile += %W( landing.css )
   end
 end
