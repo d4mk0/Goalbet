542d3726de5d16890cd18fde67d1fbdb2ac999ea
Damir Konkov <d4mk0f@gmail.com>
2014-07-16 23:05:29 +0400

    menu on main layout was centered, and now nav title and meta title established through cached settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_titles
+
+  private
+
+    def set_titles
+      set_nav_title
+      set_meta_title
+    end
+
+    def set_nav_title
+      @nav_title = Settings.nav_title
+    end
+
+    def set_meta_title
+      @meta_title = Settings['meta.title']
+    end
 end
